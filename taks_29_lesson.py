77f29d012e10641d00b92cc66daae538094a4070
Aleksandr <aleks12121993andr@gmail.com>
2024-05-08 08:42:16 +0400

    add a new solution

--- taks_29_lesson.py
+++ taks_29_lesson.py
@@ -22,39 +22,46 @@ lst = [
 ]
 
 
-def create_dict(item):
-    cl = []
-    for elem in item:
-        cl.append(dict(elem))
-    return cl
+# def create_dict(item):
+#     cl = []
+#     for elem in item:
+#         cl.append(dict(elem))
+#     return cl
+#
+#
+# sort_by_name = []
+#
+# for el in lst:
+#     sort_by_name.append(list(el.items()))
+#
+# sort_by_name.sort(key=lambda i: i[1])
+#
+# one = create_dict(sort_by_name)
+# print(one)
+#
+# sort_by_rate_up = []
+#
+# for el in lst:
+#     sort_by_rate_up.append(list(el.items()))
+#
+# sort_by_rate_up.sort(key=lambda i: i[2])
+#
+# two = create_dict(sort_by_rate_up)
+# print(two)
+#
+# sort_by_rate_down = []
+#
+# for el in lst:
+#     sort_by_rate_down.append(list(el.items()))
+#
+# sort_by_rate_down.sort(reverse=True, key=lambda i: i[2])
+#
+# three = create_dict(sort_by_rate_down)
+# print(three)
 
-
-sort_by_name = []
-
-for el in lst:
-    sort_by_name.append(list(el.items()))
-
-sort_by_name.sort(key=lambda i: i[1])
-
-one = create_dict(sort_by_name)
-print(one)
-
-sort_by_rate_up = []
-
-for el in lst:
-    sort_by_rate_up.append(list(el.items()))
-
-sort_by_rate_up.sort(key=lambda i: i[2])
-
-two = create_dict(sort_by_rate_up)
-print(two)
-
-sort_by_rate_down = []
-
-for el in lst:
-    sort_by_rate_down.append(list(el.items()))
-
-sort_by_rate_down.sort(reverse=True, key=lambda i: i[2])
-
-three = create_dict(sort_by_rate_down)
-print(three)
+res1 = sorted(lst, key=lambda i: i['lastname'])
+res2 = sorted(lst, key=lambda i: i['rating'])
+res3 = sorted(lst, reverse=True, key=lambda i: i['rating'])
+print(res1)
+print(res2)
+print(res3)
