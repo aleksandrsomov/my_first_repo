8644f55833b86309a65f030b21403e008c7b891b
Aleksandr <aleks12121993andr@gmail.com>
2024-05-16 08:48:16 +0400

    add a new homework

---
+++ 34_lesson_homework.py
@@ -0,0 +1,10 @@
+person_fullname = list(map(lambda x: x[0].upper() + x[1:].lower(), input('Введите ФИО: ').split()))
+
+for i in range(len(person_fullname)):
+    if i > 0:
+        person_fullname[i] = person_fullname[i][:1] + '.'
+
+# print(person_fullname)
+
+FIO = ' '.join(person_fullname)
+print(FIO)
